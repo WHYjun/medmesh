e2babe2b8cdb32859bcc7433e4ade27d66e73aa4
WHYjun <yjunchoi@bu.edu>
2018-09-08 14:17:12 -0400

    add integration method to handle demographics and heart rates

--- server/modules/fitbit_module.py
+++ server/modules/fitbit_module.py
@@ -1,7 +1,8 @@
 import datetime
 import fitbit
-import gather_keys_oauth2 as Oauth2
+from modules import gather_keys_oauth2 as Oauth2
 import os
+import pprint
 
 FITBIT_ID = os.environ.get('FITBIT_ID')
 FITBIT_SECRET = os.environ.get('FITBIT_SECRET')
@@ -17,19 +18,18 @@ def get_token():
     print('Update your refresh token to {}'.format(refresh_token))
 
 
-def get_heartrate():
+def get_heartrate(start=None, end=None):
     access_token = os.environ.get('ACCESS_TOKEN')
     refresh_token = os.environ.get('REFRESH_TOKEN')
     auth2_client = fitbit.Fitbit(FITBIT_ID, FITBIT_SECRET,
                                  oauth2=True,
                                  access_token=access_token,
                                  refresh_token=refresh_token)
-    yesterday = str((datetime.datetime.now() -
-                     datetime.timedelta(days=1)).strftime('%Y-%m-%d'))
     fit_statsHR = auth2_client.intraday_time_series(
-        'activities/heart', base_date=yesterday, detail_level='1sec')
-    print(fit_statsHR)
+        'activities/heart', base_date='today', detail_level='1sec',
+        start_time=start, end_time=end)
+    return fit_statsHR
 
 
 if __name__ == '__main__':
-    get_heartrate()
+    pprint.pprint(get_heartrate())
--- server/server.py
+++ server/server.py
@@ -1,23 +1,91 @@
 import fitbit
-from flask import Flask
+from flask import Flask, jsonify
+import logging
+import os
 import requests
 
 # App
 from configure import app
+
+# Log
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler('logs/server.log')
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
+
+# Modules
+from modules import fitbit_module
 
 # MongoDB
 from pymongo import MongoClient
 client = MongoClient()
 db = client.heartcare
 
-# Endpoints
-from endpoints import fitbit_endpoints
-
-app.register_blueprint(fitbit_endpoints.endpoints)
-
+# User Data
+user_data = {
+    'Amy': {
+        'age': 58.0,
+        'hypertension': 1.0,
+        'heart_disease': 1.0,
+        'bmi': 38.0,
+        'gender_numeric': 2.0,
+        'ever_married_numeric': 1.0,
+        'work_type_numeric': 1.0,
+        'residence_numeric': 2.0,
+        'smoking_status_numeric': 1.0
+    },
+    'Bob': {
+        'age': 58.0,
+        'hypertension': 1.0,
+        'heart_disease': 1.0,
+        'bmi': 38.0,
+        'gender_numeric': 2.0,
+        'ever_married_numeric': 1.0,
+        'work_type_numeric': 1.0,
+        'residence_numeric': 2.0,
+        'smoking_status_numeric': 1.0
+    },
+    'Charlie': {
+        'age': 58.0,
+        'hypertension': 1.0,
+        'heart_disease': 1.0,
+        'bmi': 38.0,
+        'gender_numeric': 2.0,
+        'ever_married_numeric': 1.0,
+        'work_type_numeric': 1.0,
+        'residence_numeric': 2.0,
+        'smoking_status_numeric': 1.0
+    }
+}
 
 # API
+@app.route('/api/user/<username>', methods=['GET'])
+def get_percentage(username):
+    user = user_data.get(username)
+    if not user:
+        logger.error('get_user({}): username not existed'.format(username))
+        return None
+    if os.environ.get('env') == 'demo':
+        # TODO: Get data from real cases
+        pass
+    else:
+        start = '13:00'
+        end = '13:01'
+        heart_rates = fitbit_module.get_heartrate(start=start, end=end)
+    req = user
+    req['heart_rate'] = calculate_hr(heart_rates['activities-heart-intraday'])
+    # percentage = requests.post('url', json=req)
+    return jsonify(req)
 
+
+def calculate_hr(heart_rates):
+    total = 0
+    for heart_rate in heart_rates['dataset']:
+        total += heart_rate['value']
+    avg = total/len(heart_rates['dataset'])
+    return round(avg, 2)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=int("8080"), debug=True)
